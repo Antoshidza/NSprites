66a3f59066b1f74ef81e507ec4b101de4c06b92c
TonyMax <antoshidza@mail.ru>
2023-01-10 13:01:48 +0300

    Turn SystemBase to ISystem

--- Rendering/Components/RenderArchetypeStorage.cs
+++ Rendering/Components/RenderArchetypeStorage.cs
@@ -8,17 +8,17 @@ namespace NSprites
     public class RenderArchetypeStorage : IComponentData
     {
         /// <summary><see cref="Mesh"/> We will use to render every sprite, which can be created once in system</summary>
-        internal Mesh _quad = NSpritesUtils.ConstructQuad();
+        internal Mesh Quad = NSpritesUtils.ConstructQuad();
         /// <summary>Shader property's id to property data map</summary>
-        internal readonly Dictionary<int, PropertyInternalData> _propetyMap = new();
+        internal readonly Dictionary<int, PropertyInternalData> PropertyMap = new();
         /// <summary>All whenever registered render archetypes. Each registred archetype will be updated every frame no matter if there is any entities.</summary>
-        internal readonly List<RenderArchetype> _renderArchetypes = new();
+        internal readonly List<RenderArchetype> RenderArchetypes = new();
         /// <summary>System's state with all necessary data to pass to <see cref="RenderArchetype"/> to update</summary>
-        internal SystemState _state;
+        internal SystemData SystemData;
 
         internal void Dispose()
         {
-            foreach (var archetype in _renderArchetypes)
+            foreach (var archetype in RenderArchetypes)
                 archetype.Dispose();
         }
         internal void Initialize() => GatherPropertiesTypes();
@@ -28,7 +28,7 @@ namespace NSprites
         /// But you can use this method to manually pass bind data.
         /// </summary>
         public void BindComponentToShaderProperty(in int propertyID, Type componentType, in PropertyFormat format)
-            => _propetyMap.Add(propertyID, new PropertyInternalData(new ComponentType(componentType, ComponentType.AccessMode.ReadOnly), format));
+            => PropertyMap.Add(propertyID, new PropertyInternalData(new ComponentType(componentType, ComponentType.AccessMode.ReadOnly), format));
         /// <summary>
         /// Binds component to shader's property. Binded components will be gathered from entities during render process to be passed to shader.
         /// By default system will automatically gather and bind all component types which have <see cref="InstancedPropertyComponent"/> attribute to specified property.
@@ -37,7 +37,7 @@ namespace NSprites
         public void BindComponentToShaderProperty(in string propertyName, Type componentType, in PropertyFormat format)
             => BindComponentToShaderProperty(Shader.PropertyToID(propertyName), componentType, format);
 
-        /// <summary>Fills <see cref="_propetyMap"/> with data of all types marked by <see cref="InstancedPropertyComponent"/> attribute</summary>
+        /// <summary>Fills <see cref="PropertyMap"/> with data of all types marked by <see cref="InstancedPropertyComponent"/> attribute</summary>
         private void GatherPropertiesTypes()
         {
             foreach (var property in InstancedPropertyComponent.GetProperties())
@@ -57,6 +57,6 @@ namespace NSprites
         /// <param name="initialCapacity">compute buffers intial capacity.</param>
         /// <param name="capacityStep">compute buffers capacity increase step when the current limit on the number of entities is exceeded.</param>
         public void RegisterRender(in int id, Material material, MaterialPropertyBlock materialPropertyBlock = null, in int initialCapacity = 1, in int capacityStep = 1, params PropertyData[] propertyDataSet)
-            => _renderArchetypes.Add(new RenderArchetype(material, propertyDataSet, _propetyMap, id, _state.system, materialPropertyBlock, initialCapacity, capacityStep));
+            => RenderArchetypes.Add(new RenderArchetype(material, propertyDataSet, PropertyMap, id, materialPropertyBlock, initialCapacity, capacityStep));
     }
 }
--- Rendering/Systems/SpriteRenderingSystem.cs
+++ Rendering/Systems/SpriteRenderingSystem.cs
@@ -10,64 +10,56 @@ namespace NSprites
     /// </summary>
     [WorldSystemFilter(WorldSystemFilterFlags.Default | WorldSystemFilterFlags.Editor)]
     [UpdateInGroup(typeof(PresentationSystemGroup))]
-    public partial class SpriteRenderingSystem : SystemBase
+    public partial struct SpriteRenderingSystem : ISystem
     {
-        protected override void OnCreate()
+        public void OnCreate(ref SystemState state)
         {
 #if NSPRITES_REACTIVE_DISABLE && NSPRITES_STATIC_DISABLE && NSPRITES_EACH_UPDATE_DISABLE
             throw new Exception($"You can't disable Reactive, Static and Each-Update properties modes at the same time, there should be at least one mode if you want system to work. Please, enable at least one mode.");
 #endif
-            base.OnCreate();
-
-            var renderArchetypeStorage = new RenderArchetypeStorage
-            {
-                _state = new SystemState
-                {
-                    system = this,
-                    query = GetEntityQuery(NSpritesUtils.GetDefaultComponentTypes())
-                }
-            };
+            // instansiate and initialize system data
+            var renderArchetypeStorage = new RenderArchetypeStorage{ SystemData = new SystemData { query = state.GetEntityQuery(NSpritesUtils.GetDefaultComponentTypes()) }};
             renderArchetypeStorage.Initialize();
+            state.EntityManager.AddComponentObject(state.SystemHandle, renderArchetypeStorage);
+        }
 
-            EntityManager.AddComponentObject(SystemHandle, renderArchetypeStorage);
-        }
-        protected override void OnDestroy()
+        public void OnDestroy(ref SystemState state)
         {
-            base.OnDestroy();
-            EntityManager.GetComponentObject<RenderArchetypeStorage>(SystemHandle).Dispose();
+            SystemAPI.ManagedAPI.GetComponent<RenderArchetypeStorage>(state.SystemHandle).Dispose();
         }
-        protected override void OnUpdate()
+
+        public void OnUpdate(ref SystemState state)
         {
-            var renderArchetypeStorage = SystemAPI.ManagedAPI.GetComponent<RenderArchetypeStorage>(SystemHandle);
+            var renderArchetypeStorage = SystemAPI.ManagedAPI.GetComponent<RenderArchetypeStorage>(state.SystemHandle);
 #if UNITY_EDITOR
-            if (!Application.isPlaying && renderArchetypeStorage._quad == null)
-                renderArchetypeStorage._quad = NSpritesUtils.ConstructQuad();
+            if (!Application.isPlaying && renderArchetypeStorage.Quad == null)
+                renderArchetypeStorage.Quad = NSpritesUtils.ConstructQuad();
 #endif
             // update state to pass to render archetypes
 #if !NSPRITES_REACTIVE_DISABLE || !NSPRITES_STATIC_DISABLE
-            var state = renderArchetypeStorage._state;
-            state.lastSystemVersion = LastSystemVersion;
-            state.propertyPointer_CTH_RW = SystemAPI.GetComponentTypeHandle<PropertyPointer>(false);
-            state.propertyPointerChunk_CTH_RW = SystemAPI.GetComponentTypeHandle<PropertyPointerChunk>(false);
-            state.propertyPointerChunk_CTH_RO = SystemAPI.GetComponentTypeHandle<PropertyPointerChunk>(true);
+            var systemData = renderArchetypeStorage.SystemData;
+            systemData.lastSystemVersion = state.LastSystemVersion;
+            systemData.propertyPointer_CTH_RW = SystemAPI.GetComponentTypeHandle<PropertyPointer>(false);
+            systemData.propertyPointerChunk_CTH_RW = SystemAPI.GetComponentTypeHandle<PropertyPointerChunk>(false);
+            systemData.propertyPointerChunk_CTH_RO = SystemAPI.GetComponentTypeHandle<PropertyPointerChunk>(true);
 #endif
-            state.inputDeps = Dependency;
+            systemData.inputDeps = state.Dependency;
 
             // schedule render archetype's properties data update
-            var renderArchetypeHandles = new NativeArray<JobHandle>(renderArchetypeStorage._renderArchetypes.Count, Allocator.Temp);
-            for (int archetypeIndex = 0; archetypeIndex < renderArchetypeStorage._renderArchetypes.Count; archetypeIndex++)
-                renderArchetypeHandles[archetypeIndex] = renderArchetypeStorage._renderArchetypes[archetypeIndex].ScheduleUpdate(state);
+            var renderArchetypeHandles = new NativeArray<JobHandle>(renderArchetypeStorage.RenderArchetypes.Count, Allocator.Temp);
+            for (int archetypeIndex = 0; archetypeIndex < renderArchetypeStorage.RenderArchetypes.Count; archetypeIndex++)
+                renderArchetypeHandles[archetypeIndex] = renderArchetypeStorage.RenderArchetypes[archetypeIndex].ScheduleUpdate(systemData, ref state);
 
             // force complete properties data update and draw archetypes
-            for (int archetypeIndex = 0; archetypeIndex < renderArchetypeStorage._renderArchetypes.Count; archetypeIndex++)
+            for (int archetypeIndex = 0; archetypeIndex < renderArchetypeStorage.RenderArchetypes.Count; archetypeIndex++)
             {
-                var archetype = renderArchetypeStorage._renderArchetypes[archetypeIndex];
+                var archetype = renderArchetypeStorage.RenderArchetypes[archetypeIndex];
                 archetype.CompleteUpdate();
-                archetype.Draw(renderArchetypeStorage._quad, new Bounds(new Vector3(0f, 0f, archetypeIndex), Vector3.one * 1000f));
+                archetype.Draw(renderArchetypeStorage.Quad, new Bounds(new Vector3(0f, 0f, archetypeIndex), Vector3.one * 1000f));
             }
 
             // combine handles from all render archetypes we have updated
-            Dependency = JobHandle.CombineDependencies(renderArchetypeHandles);
+            state.Dependency = JobHandle.CombineDependencies(renderArchetypeHandles);
         }
     }
 }
