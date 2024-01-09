dd745fc384974a77ecec4607f42daa01b4be5d85
Anton Chernykh <antoshidza@mail.ru>
2024-01-09 22:03:28 +0300

    ArchetypeStorage improvements
    * storage now test if archetype with same id already registered and log an error if it is
    * client code now can clear storage which disposes all render archetypes (for example when you change game level and no more need previous archetypes)

--- Rendering/Components/RenderArchetypeStorage.cs
+++ Rendering/Components/RenderArchetypeStorage.cs
@@ -9,11 +9,12 @@ namespace NSprites
     {
         /// <summary><see cref="Mesh"/> We will use to render every sprite, which can be created once in system</summary>
         internal Mesh Quad = NSpritesUtils.ConstructQuad();
-        /// <summary>Shader property's id to property data map</summary>
+        /// <summary> Shader property's id to property data map</summary>
         internal readonly Dictionary<int, ComponentType> PropertyMap = new();
-        /// <summary>All whenever registered render archetypes. Each registered archetype will be updated every frame no matter if there is any entities.</summary>
+        /// <summary> All whenever registered render archetypes. Each registered archetype will be updated every frame no matter if there is any entities. </summary>
         internal readonly List<RenderArchetype> RenderArchetypes = new();
-        /// <summary>System's state with all necessary data to pass to <see cref="RenderArchetype"/> to update</summary>
+        internal readonly HashSet<int> RegisteredIds = new();
+        /// <summary> System's state with all necessary data to pass to <see cref="RenderArchetype"/> to update </summary>
         internal SystemData SystemData;
         
         internal static Bounds DefaultBounds => new (Vector3.zero, Vector3.one * 1000f);
@@ -23,30 +24,44 @@ namespace NSprites
             foreach (var archetype in RenderArchetypes)
                 archetype.Dispose();
         }
+        
         internal void Initialize() => GatherPropertiesTypes();
+
+        public bool ContainsRender(in int id) => RegisteredIds.Contains(id);
+
+        public void Clear()
+        {
+            Dispose();
+            RenderArchetypes.Clear();
+            RegisteredIds.Clear();
+        }
+
         /// <summary>
         /// Binds component to shader's property. Binded components will be gathered from entities during render process to be passed to shader.
         /// By default system will automatically gather and bind all component types which have <see cref="InstancedPropertyComponent"/> attribute to specified property.
         /// But you can use this method to manually pass bind data.
         /// </summary>
-        public void BindComponentToShaderProperty(int propertyID, Type componentType)
+        public void BindComponentToShaderProperty(in int propertyID, Type componentType)
         {
-#if UNITY_EDITOR
-            if (PropertyMap.ContainsKey(propertyID))
+#if UNITY_EDITOR || DEVELOPEMENT_BUILD
+            if (PropertyMap.TryGetValue(propertyID, out var propertyComponent))
             {
-                var propertyComponent = PropertyMap[propertyID];
                 Debug.LogError($"It seems you're trying to bind multiple components to same shader property {propertyID}.\nYou're trying to add: {componentType.Name} \nAlready registered: {propertyComponent.GetManagedType().Name}");
                 return;
             }
 #endif
             PropertyMap.Add(propertyID, new ComponentType(componentType, ComponentType.AccessMode.ReadOnly));
         }
-
-        /// <summary><inheritdoc cref="BindComponentToShaderProperty(int,System.BindComponentToShaderProperty)"/></summary>
+        
+        /// <summary>
+        /// Binds component to shader's property. Binded components will be gathered from entities during render process to be passed to shader.
+        /// By default system will automatically gather and bind all component types which have <see cref="InstancedPropertyComponent"/> attribute to specified property.
+        /// But you can use this method to manually pass bind data.
+        /// </summary>
         public void BindComponentToShaderProperty(in string propertyName, Type componentType)
             => BindComponentToShaderProperty(Shader.PropertyToID(propertyName), componentType);
 
-        /// <summary>Fills <see cref="PropertyMap"/> with data of all types marked by <see cref="InstancedPropertyComponent"/> attribute</summary>
+        /// <summary> Fills <see cref="PropertyMap"/> with data of all types marked by <see cref="InstancedPropertyComponent"/> attribute </summary>
         private void GatherPropertiesTypes()
         {
             foreach (var property in InstancedPropertyComponent.GetProperties())
@@ -65,20 +80,47 @@ namespace NSprites
         /// <param name="initialCapacity">compute buffers initial capacity.</param>
         /// <param name="capacityStep">compute buffers capacity increase step when the current limit on the number of entities is exceeded.</param>
         /// </summary>
-        public void RegisterRender(int id, Material material, MaterialPropertyBlock materialPropertyBlock = null, int initialCapacity = 1, int capacityStep = 1, params PropertyData[] propertyDataSet)
+        public void RegisterRender(in int id, Material material, MaterialPropertyBlock materialPropertyBlock = null, in int initialCapacity = 1, in int capacityStep = 1, params PropertyData[] propertyDataSet)
             => RegisterRender(id, material, Quad, DefaultBounds, materialPropertyBlock, initialCapacity, capacityStep, propertyDataSet);
         
-        /// <summary><inheritdoc cref="RegisterRender(int,UnityEngine.Material,UnityEngine.MaterialPropertyBlock,int,int,NSprites.PropertyData[])"/>
-        /// <param name="bounds">render bounds.</param>
+        /// <summary>
+        /// Register render, which is combination of Material + set of StructuredBuffer property names in shader.
+        /// Every entity with <see cref="SpriteRenderID"/> component with ID value equal to passed ID, will be rendered by registered render.
+        /// Entity without instanced property component from passed properties will be rendered with uninitialized values (please, initialize entities carefully, because render with uninitialized values can lead to strange visual results).
+        /// Though you can use <b><see cref="NSPRITES_PROPERTY_FALLBACK_ENABLE"/></b> directive to enable fallback values, so any chunk without property component will pass default values.
+        /// <param name="id">ID of <see cref="SpriteRenderID"/>.<see cref="SpriteRenderID.id"/>. All entities with the same SCD will be updated by registering render archetype. Client should manage uniqueness (or not) of ids by himself.</param>
+        /// <param name="material"><see cref="Material"/> which will be used to render sprites.</param>
+        /// <param name="materialPropertyBlock"><see cref="MaterialPropertyBlock"/> you can pass if you want to do some extra overriding by yourself.</param>
+        /// <param name="propertyDataSet">IDs of StructuredBuffer properties in shader AND <see cref="PropertyUpdateMode"/> for each property.</param>
+        /// <param name="initialCapacity">compute buffers initial capacity.</param>
+        /// <param name="capacityStep">compute buffers capacity increase step when the current limit on the number of entities is exceeded.</param>
         /// </summary>
-        public void RegisterRender(int id, Material material, in Bounds bounds, MaterialPropertyBlock materialPropertyBlock = null, int initialCapacity = 1, int capacityStep = 1, params PropertyData[] propertyDataSet)
+        public void RegisterRender(in int id, Material material, in Bounds bounds, MaterialPropertyBlock materialPropertyBlock = null, in int initialCapacity = 1, in int capacityStep = 1, params PropertyData[] propertyDataSet)
             => RegisterRender(id, material, Quad, bounds, materialPropertyBlock, initialCapacity, capacityStep, propertyDataSet);
         
-        /// <summary><inheritdoc cref="RegisterRender(int,UnityEngine.Material,UnityEngine.MaterialPropertyBlock,int,int,NSprites.PropertyData[])"/>
-        /// <param name="mesh"><see cref="Mesh"/> which will be used to render sprites.</param>
-        /// <param name="bounds">render bounds.</param>
+        /// <summary>
+        /// Register render, which is combination of Material + set of StructuredBuffer property names in shader.
+        /// Every entity with <see cref="SpriteRenderID"/> component with ID value equal to passed ID, will be rendered by registered render.
+        /// Entity without instanced property component from passed properties will be rendered with uninitialized values (please, initialize entities carefully, because render with uninitialized values can lead to strange visual results).
+        /// Though you can use <b><see cref="NSPRITES_PROPERTY_FALLBACK_ENABLE"/></b> directive to enable fallback values, so any chunk without property component will pass default values.
+        /// <param name="id">ID of <see cref="SpriteRenderID"/>.<see cref="SpriteRenderID.id"/>. All entities with the same SCD will be updated by registering render archetype. Client should manage uniqueness (or not) of ids by himself.</param>
+        /// <param name="material"><see cref="Material"/> which will be used to render sprites.</param>
+        /// <param name="materialPropertyBlock"><see cref="MaterialPropertyBlock"/> you can pass if you want to do some extra overriding by yourself.</param>
+        /// <param name="propertyDataSet">IDs of StructuredBuffer properties in shader AND <see cref="PropertyUpdateMode"/> for each property.</param>
+        /// <param name="initialCapacity">compute buffers initial capacity.</param>
+        /// <param name="capacityStep">compute buffers capacity increase step when the current limit on the number of entities is exceeded.</param>
         /// </summary>
-        public void RegisterRender(int id, Material material, Mesh mesh, in Bounds bounds, MaterialPropertyBlock materialPropertyBlock = null, int initialCapacity = 1, int capacityStep = 1, params PropertyData[] propertyDataSet)
-            => RenderArchetypes.Add(new RenderArchetype(material, mesh, bounds, propertyDataSet, PropertyMap, id, materialPropertyBlock, initialCapacity, capacityStep));
+        public void RegisterRender(in int id, Material material, Mesh mesh, in Bounds bounds, MaterialPropertyBlock materialPropertyBlock = null, in int initialCapacity = 1, in int capacityStep = 1, params PropertyData[] propertyDataSet)
+        {
+#if UNITY_EDITOR || DEVELOPEMENT_BUILD
+            if (RegisteredIds.Contains(id))
+            {
+                Debug.LogError($"There is already registered archetype with id {id}. Registering same archetypes multiple times will regress performance!");
+                return;
+            }
+#endif
+            RenderArchetypes.Add(new RenderArchetype(material, mesh, bounds, propertyDataSet, PropertyMap, id, materialPropertyBlock, initialCapacity, capacityStep));
+            RegisteredIds.Add(id);
+        }
     }
 }
