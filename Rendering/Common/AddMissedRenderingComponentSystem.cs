184502dcc42c2fc6a8bd7d26b35aa62e26c6315a
Anton Chernykh <antoshidza@mail.ru>
2024-04-16 21:18:50 +0300

    Temporal fix for ChunkComponent baking issues

---
+++ Rendering/Common/AddMissedRenderingComponentSystem.cs
@@ -0,0 +1,29 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+
+namespace NSprites
+{
+    [UpdateInGroup(typeof(PresentationSystemGroup))]
+    [UpdateBefore(typeof(SpriteRenderingSystem))]
+    [WorldSystemFilter(WorldSystemFilterFlags.Default | WorldSystemFilterFlags.Editor | WorldSystemFilterFlags.EntitySceneOptimizations)]
+    public partial struct AddMissedRenderingComponentSystem : ISystem
+    {
+        private EntityQuery _query;
+        
+        [BurstCompile]
+        public void OnCreate(ref SystemState state) 
+        {
+            _query = SystemAPI.QueryBuilder()
+                .WithAll<PropertyPointer>()
+                .WithNoneChunkComponent<PropertyPointerChunk>()
+                .WithOptions(EntityQueryOptions.IncludePrefab | EntityQueryOptions.IncludeDisabledEntities | EntityQueryOptions.Default | EntityQueryOptions.IgnoreComponentEnabledState)
+                .Build();
+            
+            state.RequireForUpdate(_query);   
+        }
+        
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state) 
+            => state.EntityManager.AddChunkComponentData(_query, new PropertyPointerChunk());
+    }
+}
